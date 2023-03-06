0da9bca41e02c5d5dd6bee60f5c38b9bea71b75d
sudh-202 <87563365+sudh-202@users.noreply.github.com>
2023-03-07 00:38:56 +0530

    new site

---
+++ src/constants/constants.js
@@ -0,0 +1,130 @@
+export const projects = [
+  {
+    title: "Chat-GPT APP",
+    description:
+      "Using React, Node.js, Express & MongoDB you'll learn how to re-build a Chat-GPT MERN Application - from start to finish. The App can to exact same task the orginal chat-gpt can do.",
+    image: "/images/1.png",
+    tags: ["Mongo", "Express", "React", "Node", "OpenAI"],
+    source: "https://aichatapp-coral.vercel.app/",
+    visit: "https://github.com/sudh-202/ChatGPT-WebApp",
+    id: 0,
+  },
+
+  {
+    title: "WebRTC App",
+    description:
+      "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
+    image: "/images/3.jpg",
+    tags: ["React", "WebRTC"],
+    source: "https://metaverse-demosite.netlify.app/",
+    visit: "https://github.com/sudh-202/MetaVersre-Website",
+    id: 2,
+  },
+  {
+    title: "Lamachat",
+    description:
+      "This is a Text-chat App built with React JS ChatEngine and GoogleFirebase.",
+    image: "/images/4.jpg",
+    tags: ["React", "ChatEngine", "Firebase"],
+    source: "https://chat-app-react-two.vercel.app/",
+    visit: "https://github.com/sudh-202/Chat-App-React",
+    id: 3,
+  },
+  {
+    title: "Typing Speed Test",
+    description:
+      "This is a Typing Speed test  App built with React JS , JavaScript.",
+    image: "/images/5.png",
+    tags: ["React", "JavaScript"],
+    source: "https://typing-test-puce.vercel.app/",
+    visit: "https://github.com/sudh-202/typing_test",
+    id: 4,
+  },
+  {
+    title: "Previous Portfolio",
+    description: "This is my Previous App built with Next JS , Socket.io.",
+    image: "/images/6.jpg",
+    tags: ["Next", "Socket"],
+    source: "https://myonlyportfoliosite.netlify.app",
+    visit: "https://github.com/sudh-202/Personal-Portfolio",
+    id: 5,
+  },
+  {
+    title: "Liverr Clone",
+    description:
+      "This is a fully function clone of Liverr App built with Vite JS , Tailwind CSS.",
+    image: "/images/7.png",
+    tags: ["Vite", "Tailwind"],
+    source: "https://liverr.netlify.app/",
+    visit: "https://github.com/sudh-202/Liverr_UI",
+    id: 5,
+  },
+  {
+    title: "AI Image Genrator",
+    description:
+      "Dive into the world of artificial intelligence and  generate everything from memes and art to beautiful UI/UX designs!",
+    image:
+      "https://user-images.githubusercontent.com/87563365/214626296-fc87d43f-d31e-483b-afbf-69a6b53f9835.jpg",
+    tags: ["Vite", "Tailwind", "Cloudinary", "OpenAI"],
+    source: "https://aigeneratedimages.netlify.app/",
+    visit: "https://github.com/sudh-202/AI-Image-Genrator",
+    id: 5,
+  },
+
+  {
+    title: "t-Movies",
+    description: "Responsive React Movies App With API",
+    image: "/images/8.jpg",
+    tags: ["React", "TMDB Movie API", "Node JS"],
+    source: "https://react-api-movie-app.netlify.app/",
+    visit: "https://github.com/sudh-202/react-movie-main",
+    id: 6,
+  },
+
+  {
+    title: "3-D Model Website",
+    description:
+      "Responsive 3d portfolio Website using React and  3-D Models of Three.js",
+    image: "/images/3d.jpg",
+    tags: ["React", "Three JS", "Email JS", "Vite"],
+    source: "https://3d-model-site.netlify.app/",
+    visit: "https://github.com/sudh-202/3d-Model",
+    id: 7,
+  },
+  {
+    title: "E-Commerce",
+    description: "Comming Soon...",
+    image: "/images/comming soon.jpeg",
+    tags: ["React", "Firebase", "Redux", "Stripe", "Node JS"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 8,
+  },
+
+  {
+    title: "Blog App",
+    description: "Comming Soon...",
+    image: "/images/comming soon.jpeg",
+    tags: ["React", "Firebase", "Express JS", "Node JS"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 9,
+  },
+
+  {
+    title: "Bookings App",
+    description: "Comming Soon...",
+    image: "/images/comming soon.jpeg",
+    tags: ["React", "Firebase", "Express JS", "Node JS"],
+    source: "https://google.com",
+    visit: "https://google.com",
+    id: 10,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2020, text: "Started my journey" },
+  { year: 2021, text: "Worked as a freelance developer" },
+  { year: 2022, text: "Gain experience during internship" },
+  { year: 2023, text: "Shared my projects with the world" },
+];
